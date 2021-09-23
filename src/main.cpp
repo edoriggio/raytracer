d92b555c0e3da7b45d1314c6c2efc999c61eae3e
Edoardo Riggio <edo.riggio19@gmail.com>
2021-09-23 15:00:13 +0200

    finished assignment 1

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,7 @@ class Ray {
 public:
 	glm::vec3 origin; ///< Origin of the ray
 	glm::vec3 direction; ///< Direction of the ray
+	
 	/**
 	 Contructor of the ray
 	 @param origin Origin of the ray
@@ -49,6 +50,7 @@ struct Hit {
 class Object {
 public:
 	glm::vec3 color; ///< Color of the object
+
 	/** A function computing an intersection, which returns the structure Hit */
 	virtual Hit intersect(Ray ray) = 0;
 };
@@ -71,22 +73,38 @@ public:
 	Sphere(float radius, glm::vec3 center, glm::vec3 color) : radius(radius), center(center) {
 		this->color = color;
 	}
+
 	/** Implementation of the intersection function*/
 	Hit intersect(Ray ray) {
 		Hit hit;
 		hit.hit = false;
-		
-		/* -------------------------------------------------
-			
-		Place for your code: ray-sphere intersection. Remember to set all the fields of the hit structure:
 
-			hit.intersection =
-			hit.normal =
-			hit.distance =
-			hit.object = this;
-			
-		------------------------------------------------- */
-				
+		glm::vec3 intersection;
+
+		glm::vec3 d = ray.direction;
+		glm::vec3 c = center;
+
+		float D = sqrt(pow(glm::length(c), 2) - pow(glm::dot(c, d), 2));
+
+		if (D > radius) {
+			return hit;
+		}
+
+		float t;
+		float t1 = glm::dot(c, d) + sqrt(pow(radius, 2) - pow(D, 2));
+		float t2 = glm::dot(c, d) - sqrt(pow(radius, 2) - pow(D, 2));
+
+		t = t1 < t2 ? t1 : t2;
+		intersection = t * ray.direction;
+		
+		glm::vec3 normal = glm::normalize(intersection);
+		
+		hit.hit = true;
+		hit.distance = glm::distance(ray.origin, intersection);
+		hit.intersection = intersection;
+		hit.normal = normal;
+		hit.object = this;
+
 		return hit;
 	}
 };
@@ -122,21 +140,15 @@ glm::vec3 trace_ray(Ray ray) {
 	} else {
 		color = glm::vec3(0.0, 0.0, 0.0);
 	}
+
 	return color;
 }
 /**
  Function defining the scene
  */
 void sceneDefinition () {
-	// first sphere (Excercise 1)
-	objects.push_back(new Sphere(1.0, glm::vec3(-0,-2,8), glm::vec3(0.6, 0.9, 0.6)));
-	
-	/* -------------------------------------------------
-	 
-	Place for your code: additional sphere (Excercise 2)
-	 
-	------------------------------------------------- */
-
+	objects.push_back(new Sphere(1.0, glm::vec3(-0, -2, 8), glm::vec3(0.6, 0.9, 0.6)));
+	objects.push_back(new Sphere(1.0, glm::vec3(1, -2, 8), glm::vec3(0.6, 0.6, 0.9)));
 }
 
 int main(int argc, const char * argv[]) {
@@ -154,8 +166,8 @@ int main(int argc, const char * argv[]) {
 	Image image(width,height); // Create an image where we will store the result
 
 	for(int i = 0; i < width ; i++)
-		for(int j = 0; j < height ; j++){
-			float dx = ((- width * pixel_size) / 2) + (i * pixel_size) + (0.5 * pixel_size);
+		for(int j = 0; j < height ; j++) {
+			float dx = (((- width) * pixel_size) / 2) + (i * pixel_size) + (0.5 * pixel_size);
 			float dy = ((height * pixel_size) / 2) - (j * pixel_size) - (0.5 * pixel_size);
 			
 			glm::vec3 origin = glm::vec3(0,0,0);
@@ -168,8 +180,8 @@ int main(int argc, const char * argv[]) {
 		}
     
 	t = clock() - t;
-	cout<<"It took " << ((float)t)/CLOCKS_PER_SEC<< " seconds to render the image."<< endl;
-	cout<<"I could render at "<< (float)CLOCKS_PER_SEC/((float)t) << " frames per second."<<endl;
+	cout<<"It took " << ((float)t)/CLOCKS_PER_SEC << " seconds to render the image." << endl;
+	cout<<"I could render at " << (float)CLOCKS_PER_SEC/((float)t) << " frames per second." << endl;
     
 	// Writing the final results of the rendering
 	if (argc == 2) {
